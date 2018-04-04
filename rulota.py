6270fd6ffac6deec2b87c64d7f5fb8ea14417af7
Alexandru Miron <mironalex96@gmail.com>
2018-04-05 00:53:40 +0300

    slight cleanup

--- rulota.py
+++ rulota.py
@@ -7,31 +7,31 @@ defined_actions = ['deploy', 'halt', 'start', 'destroy']
 source_vm = 'centos'
 
 
-def deploy(args):
-    subprocess.run(['vboxmanage', 'clonevm', source_vm, '--name', args.hostname, '--register'])
+def deploy(arguments):
+    subprocess.run(['vboxmanage', 'clonevm', source_vm, '--name', arguments.hostname, '--register'])
 
 
-def halt(args):
-    subprocess.run(['vboxmanage', 'controlvm', args.hostname, 'pause'])
+def halt(arguments):
+    subprocess.run(['vboxmanage', 'controlvm', arguments.hostname, 'pause'])
 
 
-def start(args):
-    subprocess.run(['vboxmanage', 'controlvm', args.hostname, 'resume'])
+def start(arguments):
+    subprocess.run(['vboxmanage', 'controlvm', arguments.hostname, 'resume'])
 
 
-def destroy(args):
-    subprocess.run(['vboxmanage', 'unregistervm', args.hostname, '--delete'])
+def destroy(arguments):
+    subprocess.run(['vboxmanage', 'unregistervm', arguments.hostname, '--delete'])
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage='%(prog)s [options]')
-    parser.add_argument('action', metavar='1', type=str)
-    parser.add_argument('hostname', metavar='1', type=str)
+    parser = argparse.ArgumentParser(usage='%(prog)s ' + '[' + '|'.join(defined_actions) + ']' + ' hostname')
+    parser.add_argument('action', type=str)
+    parser.add_argument('hostname', type=str)
 
     args = parser.parse_args()
 
     if args.action not in defined_actions:
-        parser.print_help()
+        parser.print_usage()
         exit(0)
 
     elif args.action == 'deploy':
@@ -46,4 +46,3 @@ if __name__ == "__main__":
     elif args.action == 'destroy':
         destroy(args)
 
-    print(args)
