8a97ed9f24a57bf4009840865a0330956f92ecb2
Alexandru Miron <mironalex96@gmail.com>
2018-04-05 12:22:06 +0300

    slight refactoring

--- rulota.py
+++ rulota.py
@@ -4,12 +4,12 @@ import argparse
 import subprocess
 import os
 
-defined_actions = ['deploy', 'halt', 'start', 'destroy']
-source_vm = 'centos'
+DEFINED_ACTIONS = ['deploy', 'halt', 'start', 'destroy']
+SOURCE_VM = 'centos'
 
 
 def deploy(arguments):
-    subprocess.run(['vboxmanage', 'clonevm', source_vm, '--name', arguments.hostname, '--register'])
+    subprocess.run(['vboxmanage', 'clonevm', SOURCE_VM, '--name', arguments.hostname, '--register'])
 
     os.chdir(os.path.dirname(__file__))
     scripts_path = os.getcwd() + "/scripts"
@@ -30,17 +30,16 @@ def destroy(arguments):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage='%(prog)s ' + '[' + '|'.join(defined_actions) + ']' + ' hostname')
-    parser.add_argument('action', type=str, help='action for taget vm: ' + '|'.join(defined_actions) )
+    parser = argparse.ArgumentParser(usage='%(prog)s ' + '[' + '|'.join(DEFINED_ACTIONS) + ']' + ' hostname')
+    parser.add_argument('action', type=str, help='action for taget vm: ' + '|'.join(DEFINED_ACTIONS))
     parser.add_argument('hostname', type=str, help='name of the target vm')
     parser.add_argument('--source', type=str, help='name of the vm to be cloned, default is centos')
 
     args = parser.parse_args()
 
-    if args.action not in defined_actions:
+    if args.action not in DEFINED_ACTIONS:
         parser.print_usage()
         exit(0)
-
 
     elif args.action == 'deploy':
         deploy(args)
